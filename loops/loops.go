8cffa71edf25a946c4bd03cb2be75937677279f6
Abhinav <AbhinavSunil@hotmail.com>
2026-06-21 16:16:01 +0530

    Working on string formatted prinitng

--- loops/loops.go
+++ loops/loops.go
@@ -1,9 +1,17 @@
 package main
+
 import "fmt"
 
 func loops(){
 	for  i := 0;i<5;i++{
 		fmt.Println("loop",i)
+	}
+
+	for i:= range 10{
+		fmt.Println("Range is ",i)
+	}
+	for i,char:= range "Abhinav"{
+		fmt.Println("Range of Abhinav",i,char)
 	}
 } 
 func main(){
